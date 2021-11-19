6dcc55a0cd5ba6ffcccb88af58da1937e017960a
yutingh <yutingh@cw.com.tw>
2021-11-19 10:53:24 +0800

    變數改為中文

--- f2e/all.js
+++ f2e/all.js
@@ -22,32 +22,32 @@ axios.get(travelApi)
     data.forEach((item, idx) => {
         let industryName = item.company.industry;
         industryData.push({
-            industryName: industryName,
-            number: 0,
-            sumScore: 0,
-            avgScore: 0,
-            sumSalaryScore: 0,
-            avgSalaryScore: 0,
+            產業名稱: industryName,
+            人數: 0,
+            產業滿意度總分: 0,
+            產業滿意度平均: 0,
+            薪資滿意度總分: 0,
+            薪資滿意度平均: 0,
         })
 
         // 移除重複obj
         industryData = industryData.filter((item, index, array) =>
             index === array.findIndex((t) => (
-                t.industryName === item.industryName
+                t.產業名稱 === item.產業名稱
             ))
         )
 
         // 抓出單筆物件
-        let industry = industryData.filter(e => e.industryName === industryName)[0];
-        industry.number++;
-        industry.sumScore += Number(item.company.score);
-        industry.sumSalaryScore += Number(item.company.salary_score);
+        let industry = industryData.filter(e => e.產業名稱 === industryName)[0];
+        industry.人數++;
+        industry.產業滿意度總分 += Number(item.company.score);
+        industry.薪資滿意度總分 += Number(item.company.salary_score);
   
         // 產業薪資滿意度平均，取小數點後第二位
-        industry.avgSalaryScore = Math.round(industry.sumSalaryScore / industry.number * 100) / 100;
+        industry.薪資滿意度平均 = Math.round(industry.薪資滿意度總分 / industry.人數 * 100) / 100;
 
         // 產業滿意度的平均，取小數點後第二位
-        industry.avgScore = Math.round(industry.sumScore / industry.number * 100) / 100;
+        industry.產業滿意度平均 = Math.round(industry.產業滿意度總分 / industry.人數 * 100) / 100;
 
     })
     
