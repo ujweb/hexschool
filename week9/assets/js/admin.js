dcc6ce1c804f5dc6af1809720a5a689df0cb7176
yutingh <yutingh@cw.com.tw>
2021-12-03 12:03:18 +0800

    swiper

--- week9/assets/js/admin.js
+++ week9/assets/js/admin.js
@@ -6,3 +6,14 @@ const adminOrders = `${domain}/admin/iris/orders`;
 
 const uid = 'rUTMZQqsKWa2mYQamfURiNOB7Yx1';
 
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
