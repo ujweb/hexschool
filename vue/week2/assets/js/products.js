2c600fa0ce08c93b5805b602bdcf70bd251004e8
ujweb <53999525+ujweb@users.noreply.github.com>
2022-01-20 15:24:20 +0800

    登入、產品頁

---
+++ vue/week2/assets/js/products.js
@@ -0,0 +1,47 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
+const app = createApp({
+    data() {
+        return {
+            api: {
+                url: 'https://vue3-course-api.hexschool.io/v2',
+                path: 'ujhwang'
+            },
+            products: [],
+            loaded: false,
+            temp: {},
+        }
+    },
+    mounted() {
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)signinToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+        axios.defaults.headers.common.Authorization = token;
+        this.checkSignin();
+    },
+    methods: {
+        checkSignin() {
+            const checkUserApi = `${this.api.url}/api/user/check`
+            axios.post(checkUserApi)
+                .then((response) => {
+                    // console.log(response);
+                    this.getProduct();
+                })
+                .catch((error) => {
+                    console.log(error);
+                    window.location = 'index.html'; // 錯誤返回登入首頁
+                })
+        },
+        getProduct() {
+            const adminProductsUrl = `${this.api.url}/api/${this.api.path}/admin/products`;
+            axios.get(adminProductsUrl)
+                .then((response) => {
+                    // console.log(response);
+                    this.products = response.data.products
+                    this.loaded = true
+                })
+                .catch((error) => {
+                    console.log(error);
+                    window.location = 'index.html'; // 錯誤返回登入首頁
+                })
+        },
+    },
+})
+app.mount('#app')
